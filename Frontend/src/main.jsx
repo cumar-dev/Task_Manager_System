9233e884ac036ff3243fa39e2a251ff1865498ef
cumar-dev <your_email@example.com>
2026-06-20 20:42:55 +0300

    setup of frontend and backend

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,10 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-
-createRoot(document.getElementById('root')).render(
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import {
+  QueryClient,
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
+const QueryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 60 * 1000,
+    },
+  },
+});
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider>
+      <App />
+    </QueryClientProvider>
   </StrictMode>,
-)
+);
